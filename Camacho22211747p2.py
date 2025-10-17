a382ea178930d3c6491858493dfb4ed17604c3e2
nicolecq04 <l22211747@tectijuana.edu.mx>
2025-10-16 21:23:30 -0700

    Add files via upload

---
+++ Camacho22211747p2.py
@@ -0,0 +1,121 @@
+"""
+Práctica 2: Sistema Cardiovascular
+
+Departamento de Ingeniería Eléctrica y Electrónica, Ingeniería Biomédica
+Tecnológico Nacional de México [TecNM - Tijuana]
+Blvd. Alberto Limón Padilla s/n, C.P. 22454, Tijuana, B.C., México
+
+Nombre del alumno: Nicole Zoe Camacho Quezada
+Número de control: 22211747
+Correo institucional: L22211747@tectijuana.edu.mx
+
+Asignatura: Modelado de Sistemas Fisiológicos
+Docente: Dr. Paul Antonio Valle Trujillo; paul.valle@tectijuana.edu.mx
+"""
+# Instalar librerias en consola
+#!pip install control
+#!pip install slycot
+import control
+
+# Librerías para cálculo numérico y generación de gráficas
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import pandas as pd
+
+u = np.array(pd.read_excel('signal.xlsx', header=None))
+x0,t0,tf,dt,w,h = 0,0,10,1E-3,10,5
+N = round((tf-t0)/dt) + 1
+t = np.linspace(t0,tf,N)
+u = np.reshape(signal.resample(u, len(t)),-1)
+
+def cardio(Z,C,R,L):
+    num = [L*R,R*Z]
+    den = [C*L*R*Z,L*R+L*Z,R*Z]
+    sys = control.tf(num,den)
+    return sys
+
+# Funcion de transferencia: Individuo normotenso (control)
+Z,C,R,L = 0.033,1.5,0.95,0.01
+sysnormo = cardio(Z,C,R,L)
+print(f'Individuo normotenso (control): {sysnormo}')
+
+# Funcion de transferencia: Individuo hipotenso (control)
+Z,C,R,L = 0.02,0.250,0.6,0.005
+syshipo = cardio(Z,C,R,L)
+print(f'Individuo hipotenso (caso): {syshipo}')
+
+# Funcion de transferencia: Individuo hipertenso (control)
+Z,C,R,L = 0.05,2.5,1.4,0.02
+syshiper = cardio(Z,C,R,L)
+print(f'Individuo hipertenso (caso): {syshiper}')
+
+# Respuestas en lazo abierto
+_, Pp0 = control.forced_response(sysnormo,t,u,x0)
+_, Pp1 = control.forced_response(syshipo,t,u,x0)
+_, Pp2 = control.forced_response(syshiper,t,u,x0)
+
+clr1 = np.array([119, 190, 240])/255
+clr2 = np.array([255, 203, 97])/255
+clr3 = np.array([255, 137, 79])/255
+clr4 = np.array([138, 166, 36])/255
+clr5 = np.array([92, 47, 194])/255
+clr6 = np.array([234, 91, 111])/255
+
+fg1 = plt.figure();
+plt.plot(t,Pp0,'-', linewidth = 1, color = clr1, label ='Pp(t):Normotenso)')
+plt.plot(t,Pp1,'-', linewidth = 1, color = clr2, label ='Pp(t):Hipotenso)')
+plt.plot(t,Pp2,'-', linewidth = 1, color = clr3, label ='Pp(t):Hipertenso)')
+plt.grid(False)
+plt.xlim(0,10); plt.xticks(np.arange(0,11,1))
+plt.ylim(-0.6,1.4); plt.yticks(np.arange(-0.6,1.6,0.2))
+plt.xlabel('t[s]')
+plt.ylabel('Pp(t) [V]')
+plt.legend(bbox_to_anchor=(0.5,-0.2),loc='center',ncol=3)
+plt.show()
+fg1.set_size_inches(w,h)
+fg1.tight_layout()
+fg1.savefig('sistema cardiovascular python.png',dpi=600,bbox_inches='tight')
+fg1.savefig('sistema cardiovascular python.pdf',bbox_inches='tight')
+
+def controlador(kP,kI):
+    Cr = 1E-6
+    Re = 1/(kI*Cr)
+    Rr = kP*Re
+    numPI = [Rr*Cr,1]
+    denPI = [Re*Cr,0]
+    PI = control.tf(numPI,denPI)
+    return PI
+
+PI = controlador(10,157446.391349792)
+X = control.series(PI,syshipo)
+hipo_PI = control.feedback(X,1,sign=-1)
+
+
+PI = controlador(100,4216882.30098903)
+X = control.series(PI,syshiper)
+hiper_PI = control.feedback(X,1,sign=-1)
+
+_, Pp3 = control.forced_response(hipo_PI,t,Pp0,x0)
+_, Pp4 = control.forced_response(hiper_PI,t,Pp0,x0)
+
+#Respuestas en lazo cerrado
+fg2 = plt.figure();
+plt.plot(t,Pp0,'-', linewidth = 1, color = clr1, label ='Pp(t):Normotenso)')
+plt.plot(t,Pp1,'-', linewidth = 1, color = clr2, label ='Pp(t):Hipotenso)')
+plt.plot(t,Pp2,'-', linewidth = 1, color = clr3, label ='Pp(t):Hipertenso)')
+plt.plot(t,Pp3,':', linewidth = 2, color = clr4, label ='Pp(t):Hipotenso PI)')
+plt.plot(t,Pp4,'-.', linewidth = 2, color = clr5, label ='Pp(t):Hipertenso PI)')
+plt.grid(False)
+plt.xlim(0,10); plt.xticks(np.arange(0,11,1))
+plt.ylim(-0.6,1.4); plt.yticks(np.arange(-0.6,1.6,0.2))
+plt.xlabel('t[s]')
+plt.ylabel('Pp(t) [V]')
+plt.legend(bbox_to_anchor=(0.5,-0.2),loc='center',ncol=5)
+plt.show()
+fg2.set_size_inches(w,h)
+fg2.tight_layout()
+fg2.savefig('sistema cardiovascular PI python.png',dpi=600,bbox_inches='tight')
+fg2.savefig('sistema cardiovascular PI python.pdf',bbox_inches='tight')
+
+
